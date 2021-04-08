e8b9642c24ebf789b2a76e2ba850e78bddc8f34d
Trevor Benyack <trevorbenyack@gmail.com>
2021-04-07 22:27:16 -0400

    finishes typing challenge project, begins pipes example

--- 03-typing/typing/src/app/app.component.ts
+++ 03-typing/typing/src/app/app.component.ts
@@ -9,5 +9,19 @@ import { lorem } from 'faker';
 export class AppComponent {
   
 randomText = lorem.sentence();
+enteredText = '';
+
+onInput(value: string) {
+  console.log(value);
+  this.enteredText = value;
+}
+
+compare(randomLetter: string, enteredLetter: string) {
+  if(!enteredLetter) {
+    return 'pending';
+  }
+
+  return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+}
 
 }
